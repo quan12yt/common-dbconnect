49312478b89fb62e2150be123e98f2cdf4f765fd
Quan Pham <quan.pham@s3corp.com.vn>
2021-03-26 10:32:38 +0700

    port changed

--- src/main/java/com/quan12yt/demo/CommonDataSource.java
+++ src/main/java/com/quan12yt/demo/CommonDataSource.java
@@ -9,7 +9,7 @@ public class CommonDataSource {
 
     public static DataSource getDataSource() throws SQLException {
         return DataSourceBuilder.create()
-                .url("jdbc:postgresql://199.30.30.98:5432/postgres")
+                .url("jdbc:postgresql://199.30.30.57:5432/postgres")
                 .password("s3training")
                 .username("postgres")
                 .driverClassName("org.postgresql.Driver")
