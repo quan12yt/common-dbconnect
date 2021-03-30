b13ea134fc0593d0a29bdebe9f52d03bf833af83
Quan Pham <quan.pham@s3corp.com.vn>
2021-03-30 11:42:00 +0700

    changed spring datasource to hikari commit

--- src/main/java/com/quan12yt/demo/CommonDataSource.java
+++ src/main/java/com/quan12yt/demo/CommonDataSource.java
@@ -1,9 +1,14 @@
 package com.quan12yt.demo;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+
 
 public class CommonDataSource {
 
@@ -14,5 +19,18 @@ public class CommonDataSource {
                 .username("postgres")
                 .driverClassName("org.postgresql.Driver")
                 .build();
+    }
+
+    public DataSource getHirakiDataSource(){
+        HikariConfig configuration = null;
+        configuration.setJdbcUrl("jdbc:postgresql://199.30.30.57:5432/postgres");
+        configuration.setDriverClassName("org.postgresql.Driver");
+        configuration.setUsername("postgres");
+        configuration.setPassword("s3training");
+        configuration.setMaximumPoolSize(2);
+        try (HikariDataSource hikariDataSource = new HikariDataSource(configuration)) {
+            return hikariDataSource;
+        }
+
     }
 }
